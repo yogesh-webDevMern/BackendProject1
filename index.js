a7b44043146f5f3b6319d4a1cbd8061596fc35d8
yogesh sharma <yogeshsharma2003kh@gmail.com>
2025-02-10 12:16:19 +0530

    final

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ app.post("/submit",(req,res)=>
 })
 })
 
-// app.listen(3000,()=>
-// {
-//     console.log("server is running on port 3000");
-// })
-module.exports = app;
+app.listen(3000,()=>
+{
+    console.log("server is running on port 3000");
+})
